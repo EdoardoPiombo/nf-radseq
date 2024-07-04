707184388ca8631c62261537c9bb571f23cf5f1c
EdoardoPiombo <edoardo.piombo@gmail.com>
2024-07-04 11:09:23 +0200

    Modified script to get metadata and the gitignore file now ignores the work directory

--- get_metadata.sh
+++ get_metadata.sh
@@ -1,6 +1,6 @@
 for i in $(ls data/fastq/ | sed 's/_L006_R.*//g' | sort -u) ; do
-readlink -f data/fastq/"$i"_L006_R1_001.fastq.gz >> column1.txt
-readlink -f data/fastq/"$i"_L006_R2_001.fastq.gz >> column2.txt
+basename data/fastq/"$i"_L006_R1_001.fastq.gz | sed 's/^/\.\/data\/fastq\//g' >> column1.txt
+basename data/fastq/"$i"_L006_R2_001.fastq.gz | sed 's/^/\.\/data\/fastq\//g' >> column2.txt
 grep "$i" doc/metadata.txt | cut -f 2 >> column3.txt
 grep "$i" doc/metadata.txt | cut -f 3 >> column4.txt
 grep "$i" doc/metadata.txt | cut -f 4 >> column5.txt
